64fa53c4564bbf0890a2f39d3ee7301a5cf49d78
lijun <lijun@weibangong.com>
2016-12-07 22:20:49 +0800

    rename package

--- app/src/main/java/com/ivan/github/MainActivity.java
+++ app/src/main/java/com/ivan/github/MainActivity.java
@@ -1,4 +1,4 @@
-package com.github;
+package com.ivan.github;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.github.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
