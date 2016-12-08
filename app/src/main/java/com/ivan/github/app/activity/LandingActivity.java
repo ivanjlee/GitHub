6e7567fca99720c76541022e6061c1b29f6033ca
lijun <lijun@weibangong.com>
2016-12-08 23:14:27 +0800

    change landing page to full screen

--- app/src/main/java/com/ivan/github/app/activity/LandingActivity.java
+++ app/src/main/java/com/ivan/github/app/activity/LandingActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.ivan.github.R;
 import com.ivan.github.app.BaseActivity;
@@ -13,6 +15,8 @@ public class LandingActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_landing);
 
         Handler handler = new Handler(getMainLooper()) {
