5decccfb1d983e141fc24583aa14d5ca5b389c9b
lijun <lijun@weibangong.com>
2016-12-10 14:36:43 +0800

    change test package name

--- app/src/androidTest/java/com/ivan/github/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ivan/github/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.github;
+package com.ivan.github;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
--- app/src/test/java/com/ivan/github/ExampleUnitTest.java
+++ app/src/test/java/com/ivan/github/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.github;
+package com.ivan.github;
 
 import org.junit.Test;
 
