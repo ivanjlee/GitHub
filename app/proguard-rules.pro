879f3922ec42255d6063239e8d7612777832680f
Ivan J. Lee <lijundut@foxmail.com>
2018-12-25 01:00:24 +0800

    profile username and email

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -17,3 +17,14 @@
 }
 
 -keepclasseswithmembers class * extends NetModel
+
+#Glide settings
+-keep public class * implements com.bumptech.glide.module.GlideModule
+-keep public class * extends com.bumptech.glide.module.AppGlideModule
+-keep public enum com.bumptech.glide.load.ImageHeaderParser$** {
+  **[] $VALUES;
+  public *;
+}
+
+# for DexGuard only
+-keepresourcexmlelements manifest/application/meta-data@value=GlideModule
--- app/src/main/java/com/ivan/github/app/main/MainActivity.java
+++ app/src/main/java/com/ivan/github/app/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ivan.github.app.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -10,38 +11,74 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.ivan.github.R;
+import com.ivan.github.account.Account;
+import com.ivan.github.account.model.User;
 import com.ivan.github.app.BaseActivity;
+import com.ivan.github.app.login.SplashActivity;
 
+/**
+ * Home Page of the App
+ */
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-public class MainActivity extends BaseActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+    private ViewGroup mProfileBackgroud;
+    private ImageView mIVAvatar;
+    private TextView mTVUsername;
+    private TextView mTVEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = this.findViewById(R.id.fab);
 //        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show());
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        View headerView = navigationView.getHeaderView(0);
+        mProfileBackgroud = headerView.findViewById(R.id.ll_profile_background);
+        mIVAvatar = headerView.findViewById(R.id.iv_avatar);
+        mTVUsername = headerView.findViewById(R.id.tv_username);
+        mTVEmail = headerView.findViewById(R.id.tv_email);
+
+        User user = Account.getInstance().getUser();
+        if (user != null) {
+            Glide.with(this)
+                    .load(user.getAvatarUrl())
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(mIVAvatar);
+            mTVUsername.setText(user.getName());
+            mTVEmail.setText(user.getEmail());
+        } else {
+            startActivity(new Intent(MainActivity.this, SplashActivity.class));
+            this.finish();
+        }
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -91,7 +128,7 @@ public class MainActivity extends BaseActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
